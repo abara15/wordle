53e2745684a5789f4b5f3cb433e0bc59c480cac2
abara15 <61340421+abara15@users.noreply.github.com>
2022-03-26 00:11:25 +1100

    created basic keyboard

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,8 +1,28 @@
 import React from 'react'
 
 function Keyboard() {
+    const l1_keys = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+    const l2_keys = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+    const l3_keys = ["Z", "X", "C", "V", "B", "N", "M"];
+
     return (
-        <div>Keyboard</div>
+        <div className='keyboard'>
+            <div className='line1'>
+                {l1_keys.map((key, index) => (
+                    <div>{key}</div>
+                ))}
+            </div>
+            <div className='line2'>
+                {l2_keys.map((key, index) => (
+                    <div>{key}</div>
+                ))}
+            </div>
+            <div className='line3'>
+                {l3_keys.map((key, index) => (
+                    <div>{key}</div>
+                ))}
+            </div>
+        </div>
     );
 }
 
