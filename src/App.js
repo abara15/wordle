a82bb126de20968cab3c34905d574efecdd82eca
abara15 <61340421+abara15@users.noreply.github.com>
2022-05-13 22:54:37 +1000

    generate word and guess sets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import { Container } from './components';
 import './App.scss';
+import { useContext, useEffect } from 'react';
+import { generateGuessSet, generateWordSet } from './constants/Words';
+import { Context } from './context/Store';
 
 function App() {
+
+	const [state, setState] = useContext(Context);
+
+	useEffect(() => {
+		generateWordSet().then((words) => {
+			setState(prevState => ({
+				...prevState,
+				correctWord: words.gameWord,
+				wordSet: words.wordSet
+			}));
+		});
+
+		generateGuessSet().then((guesses) => {
+			setState(prevState => ({
+				...prevState,
+				guessSet: guesses.guessSet
+			}));
+		});
+	}, []);
+
+	useEffect(() => {
+		console.log(state.correctWord);
+	});
+
 	return (
 		<div className="App">
 			<Container />
