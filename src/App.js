3bd2e41a0ab92ac28f2d835cc58fa7d2281fbed1
abara15 <61340421+abara15@users.noreply.github.com>
2022-05-13 20:16:03 +1000

    built new design for app

--- src/App.js
+++ src/App.js
@@ -1,105 +1,12 @@
-import { createContext, useEffect, useState } from 'react';
-import './App.css';
-import { Board, Keyboard, GameOver, Navbar } from './components';
-import { boardDefault, generateGuessSet, generateWordSet } from './constants/Words';
+import { Container } from './components';
+import './App.scss';
 
-export const AppContext = createContext();
-
-export default function App() {
-	// Initialize the board with its default state
-    const [board, setBoard] = useState(boardDefault);
-	const [currAttempt, setCurrAttempt] = useState({attempt: 0, letterPos: 0});
-	const [wordSet, setWordSet] = useState(new Set());
-	const [guessSet, setGuessSet] = useState(new Set());
-	const [disabledLetters, setDisabledLetters] = useState([]);
-	const [almostLetters, setAlmostLetters] = useState([]);
-	const [correctLetters, setCorrectLetters] = useState([]);
-	const [gameOver, setGameOver] = useState({
-		gameOver: false,
-		guessedWord: false
-	});
-	const [correctWord, setCorrectWord] = useState("");
-
-	useEffect(() => {
-		generateWordSet().then((words) => {
-			setCorrectWord(words.gameWord);
-			setWordSet(words.wordSet);
-		});
-		generateGuessSet().then((guesses) => {
-			setGuessSet(guesses.guessSet);
-		});
-	}, [])
-
-	const onSelectLetter = (keyVal) => {
-		const newBoard = [...board];
-		if (currAttempt.letterPos > 4) return;
-		newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
-		setBoard(newBoard);
-		setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos + 1});
-	};
-
-	const onDelete = () => {
-		const newBoard = [...board];
-		if (currAttempt.letterPos === 0) return;
-		newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
-		setBoard(newBoard);
-		setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos - 1});
-	};
-	
-	const onEnter = () => {
-		console.log(correctWord);
-		if (currAttempt.letterPos !== 5) return;
-
-		let currWord = "";
-		for (let i = 0; i < 5; i++) {
-			currWord += board[currAttempt.attempt][i];
-		}
-
-		if (wordSet.has(currWord.toLowerCase()) || guessSet.has(currWord.toLowerCase())) {
-			setCurrAttempt({attempt: currAttempt.attempt + 1, letterPos: 0});
-		} else {
-			alert("Word not found");
-		}
-
-		if (currWord === correctWord.toUpperCase()) {
-			setGameOver({gameOver: true, guessedWord: true});
-			return;
-		}
-
-		if (currAttempt.attempt === 5) {
-			setGameOver({gameOver: true, guessedWord: false});
-		}
-	};
-
+function App() {
 	return (
 		<div className="App">
-			<Navbar />
-			<AppContext.Provider 
-				value={{
-					board,
-					setBoard,
-					currAttempt,
-					setCurrAttempt,
-					onSelectLetter,
-					onDelete,
-					onEnter,
-					disabledLetters,
-					setDisabledLetters,
-					almostLetters,
-					setAlmostLetters,
-					correctLetters,
-					setCorrectLetters,
-					gameOver,
-					setGameOver,
-					correctWord
-				}}
-			>
-				<div className='game'>
-					{gameOver.gameOver ? <GameOver /> : ""}
-					<Board />
-					<Keyboard />
-				</div>
-			</AppContext.Provider>
+			<Container />
 		</div>
 	);
 }
+
+export default App;
---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,10 @@
+import Header from '../Header/Header';
+import './Container.scss';
+
+export default function Container() {
+    return (
+        <div className='app-container'>
+            <Header />
+        </div>
+    );
+}
