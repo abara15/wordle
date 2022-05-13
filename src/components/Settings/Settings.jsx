d339d3e5562624afe993ea0ae2130aac1605806c
abara15 <61340421+abara15@users.noreply.github.com>
2022-05-13 23:39:07 +1000

    added drawer to settings component

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,12 +1,37 @@
-import { IconButton } from "@mui/material";
+import { Box, Drawer, IconButton } from "@mui/material";
+import { useState } from "react";
 import { BiCog } from 'react-icons/bi';
 
 export default function Settings() {
+
+    const [open, setOpen] = useState(false);
+
+    const toggleDrawer = (toggle) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) return;
+        setOpen(toggle);
+    };
+
     return (
         <>
-            <IconButton aria-label="settings button">
+            <IconButton
+                onClick={toggleDrawer(true)}
+                aria-label="settings button"
+            >
                 <BiCog color="#ffffff" />
             </IconButton>
+            <Drawer
+                anchor='right'
+                open={open}
+                onClose={toggleDrawer(false)}
+            >
+                <Box
+                    sx={{ width: 250, backgroundColor: '#111111' }}
+                    role="presentation"
+                    onClick={toggleDrawer(false)}
+                    onKeyDown={toggleDrawer(false)}
+                >
+                </Box>
+            </Drawer>
         </>
     );
 }
