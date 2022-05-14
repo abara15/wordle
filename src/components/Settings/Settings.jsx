f83f59d70be6e462390434d4099ad0ac02eebfcd
abara15 <61340421+abara15@users.noreply.github.com>
2022-05-14 20:00:51 +1000

    added toggle to menu - no functionality

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,14 +1,20 @@
-import { Box, Drawer, IconButton } from "@mui/material";
+import { Box, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText, Switch } from "@mui/material";
 import { useState } from "react";
 import { BiCog } from 'react-icons/bi';
+import { IoContrastOutline } from 'react-icons/io5';
 
 export default function Settings() {
 
     const [open, setOpen] = useState(false);
+    const [checked, setChecked] = useState(false);
 
     const toggleDrawer = (toggle) => (event) => {
         if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) return;
         setOpen(toggle);
+    };
+
+    const handleToggle = () => {
+        setChecked(!checked);
     };
 
     return (
@@ -25,11 +31,28 @@ export default function Settings() {
                 onClose={toggleDrawer(false)}
             >
                 <Box
-                    sx={{ width: 250, backgroundColor: '#111111' }}
+                    sx={{ width: 250 }}
                     role="presentation"
                     onClick={toggleDrawer(false)}
                     onKeyDown={toggleDrawer(false)}
                 >
+                    <List>
+                        <ListItem>
+                            <ListItemIcon>
+                                <IoContrastOutline />
+                            </ListItemIcon>
+                            <ListItemText id="switch-list-label-contrast" primary="High Contrast Mode" />
+                            <Switch
+                                edge='end'
+                                onChange={handleToggle}
+                                checked={checked}
+                                disabled
+                                inputProps={{
+                                    'aria-labelledby': 'switch-list-label-contrast',
+                                }}
+                            />
+                        </ListItem>
+                    </List>
                 </Box>
             </Drawer>
         </>
